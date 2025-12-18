38a997b109a21d7e3f51c65c5c50cefa149949fd
OnkarJondhale <onkar.jondhale@gmail.com>
2025-12-19 03:25:26 +0530

    Add build for client

--- docker/server/server.js
+++ docker/server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
-
 const { proxyRouter } = require('./route/proxy-router');
 
 app.use(cors());
 app.use(express.json());
 
 app.use(proxyRouter);
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(9090);
